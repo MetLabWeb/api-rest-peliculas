2cc545a3441b11aa7eb41a277ff90141e1d1c53c
Sabrina del Poy <sabrina.delpoy@auctane.com>
2024-08-31 09:57:06 +0200

    agregar routes

--- src/movies/movies.routes.ts
+++ src/movies/movies.routes.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express";
-import { Movie, MovieToSave } from "./movies.types";
+import express from "express";
+import { MovieToSave } from "./movies.types";
 import * as database from "./movies.database";
 import { StatusCodes } from "http-status-codes";
 
@@ -7,4 +7,60 @@ export const moviesRouter = express.Router();
 
 moviesRouter.get("/test", (_req, res) => {
   res.status(StatusCodes.OK).json("Hola mundo");
+});
+
+moviesRouter.get("/movies", async (req, res) => {
+  const genre = req.query.genre as string;
+  if (genre) {
+    const movies = await database.findByGenre(genre);
+    if (!movies) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: `Movies with genre ${genre} not found` });
+    }
+    return res.status(StatusCodes.OK).json(movies);
+  } else {
+    const movies = await database.findAll();
+    return res.status(StatusCodes.OK).json(movies);
+  }
+});
+
+moviesRouter.get("/movies/:id", async (req, res) => {
+  const movie = await database.findById(Number(req.params.id));
+
+  if (!movie) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ error: `Movie with id ${req.params.id} not found` });
+  }
+
+  return res.status(StatusCodes.OK).json(movie);
+});
+
+moviesRouter.post("/movies", async (req, res) => {
+  const movie = req.body as MovieToSave;
+  if (!movie) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: `Please provide all the required parameters..` });
+  }
+  const newMovie = await database.create(req.body);
+  return res.status(StatusCodes.CREATED).json(newMovie);
+});
+
+moviesRouter.patch("/movies/:id", async (req, res) => {
+  const newMovie = await database.update(Number(req.params.id), req.body);
+  return res.status(StatusCodes.OK).json(newMovie);
+});
+
+moviesRouter.delete("/movies/:id", async (req, res) => {
+  const deleteMovie = await database.remove(Number(req.params.id));
+
+  if (!deleteMovie) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ error: `No movie with ID ${req.params.id}` });
+  }
+
+  return res.status(StatusCodes.OK).json({ msg: `Movie deleted..` });
 });
